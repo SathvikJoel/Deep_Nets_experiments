0b29a3df753e7992f55a862ac7c8de2f76fc79ed
Sathvik Joel <ksjoe30@gmail.com>
2020-08-25 01:18:00 +0530

    written templaye test script v1

---
+++ src/lib/test_lib.py
@@ -0,0 +1,64 @@
+import torch
+from  datetime import datetime
+from pathlib import Path
+
+def print_args(args, f_output , csv_output):
+
+    args_dict = args.__dict__
+    keys = args_dict.keys()
+
+    #print date and time
+    now = datetime.datetime.now()
+    print (now.strftime("%Y-%m-%d %H:%M:%S"), file = f_output)
+    print("****************************************", file=f_output)
+    print("****************************************", file=f_output)
+    for key in keys:
+        print(f"{key}: {args_dict[key]}", file=f_output)
+    print("****************************************", file=f_output)
+    print("****************************************\n\n\n", file=f_output)
+
+    print("*****************NOTES***********************", file=f_output)
+    print('\n\n', file = f_output)
+    print("*****************NOTES***********************", file=f_output)
+
+
+#converts the time in learn.recorder.log to flaot for furthur computation
+def to_int(logList):
+    logList[-1] = float('.'.join(logList[-1].split(':')))
+    return logList
+
+def mean_std(log,training_time):
+    log = torch.Tensor(log)
+    log_mean, log_std = torch.mean(log , dim = 0), torch.std(log, dim = 0)
+    training_time = torch.tenosr(training_time)
+    mean_training_time = torch.mean(training_time)
+    std_training_time = torch.std(training_time) 
+    return log_mean , log_std , mean_training_time , std_training_time
+
+def write_stats(args, f_output, log , training_time, exe_time):
+    log_mean , log_std , mean_training_time , std_training_time = mean_std(log,training_time)
+    # Writing statistics to file
+    print("******************STATS**********************", file=f_output)
+    print("Model: ", args.model, file=f_output)
+    print(f"epochs: {log_mean[0]}", file = f_output)
+    print(f"Mean train loss: {log_mean[1]} +- {log_std[1]}", file=f_output)
+    print(f"Mean valid loss: {log_mean[2]} +- {log_std[2]}", file=f_output)
+    print(f"Mean train   accuracy: {log_mean[3]} +- {log_mean[3]}\n", file=f_output)
+
+    print(f"Mean epoch time: {log_mean[4]} +- {log_std[4]}", file=f_output)
+    print(f"Mean training time(per run): {mean_training_time} +- {std_training_time}", file=f_output)
+    print(f"Total execuation time taken:{exe_time}", file = f_output)
+    print("****************************************\n\n\n", file=f_output)
+
+'''
+If run == 0 , then it adds a new line to the csv output to mark the start
+'''
+def write_new_line(csv_output, args):
+    now = datetime.datetime.now()
+    sheet = Path(csv_output).open('a+')
+    sheet.write(','.join(['NEW_EXP', now.strftime("%Y-%m-%d %H:%M:%S")]) + '\n')
+    sheet.write(','.join(args.keys()) + '\n')
+    sheet.write(','.join([str(t) for t in args.values()]) + '\n')
+
+#Returns str 
+def to_string(a): return str(a)
---
+++ src/scripts/template_script.py
@@ -0,0 +1,117 @@
+'''
+This is meant to be a template for all the test scripts
+'''
+import sys
+sys.path.insert(1, 'src\lib')         
+from argparse import ArgumentParser , SUPPRESS
+import os
+import re
+import time
+import pickle
+from  datetime import datetime
+from fastai.vision.all import *
+from fastai import *
+from csv import writer
+from test_lib import *
+
+'''
+Function to parse the input
+'''
+def parse_input():
+    
+    parser = ArgumentParser(add_help=False)
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    # Add back help 
+    optional.add_argument(
+        '-h',
+        '--help',
+        action='help',
+        default=SUPPRESS,
+        help='Enter all required fields'
+    )
+    required.add_argument('-d', '--dataset', type=str, metavar='STRING', choices=('IMAGENETTE', 'IMAGEWOOF'), help='dataset, choices {%(choices)s}', required=True)
+    required.add_argument('-m', '--model_name', type=str, metavar='STRING', help='model name', required= True)
+    required.add_argument('-e', '--num_epochs', type=int, metavar='NUMBER', help='number of training epochs', required= True)
+    required.add_argument('-l', '--learning_rate', type=float, metavar='NUMBER', help='base learning rate',required= True)
+    required.add_argument('-c', '--cortex', type = int , default = 20 , metavar='NUMBER', help = 'synergy number', required=True)
+    required.add_argument('-lab', '--laboratory', type = str, metavar='STRING', help='Experiment Number string',required=True)
+
+    optional.add_argument('-s', '--size', type = int , metavar = 'STRING', default = 128 , help = 'Size of image(default:128')
+    optional.add_argument('-r', '--repetitions', type=int, metavar='NUMBER', default=1, help='number of repetitions')
+    required.add_argument('-b', '--batch_size', type=int, default=128, metavar='NUMBER', help='batch size(default:128)')
+    args = parser.parse_args()
+
+    return args
+
+
+'''
+Creates required directories and files
+'''
+def setup():
+    args = parse_input()
+    path = os.getcwd()      
+    #path to lab_xx folder
+    out_path = os.path.join(path , 'results', 'coretex_'+ str(args.cortex), 'lab_' + str(args.experiment))
+
+    filepath= os.join(out_path, args.dataset, args.model_name, os.path.sep)
+
+    #create folder corresponding to modle_name and dataset
+    if not os.path.exists(filepath):
+        print(f"Generating folder {filepath}")
+        os.makedirs(filepath)
+    
+    #output file for storing summary
+    f_output = open(filepath +args.dataset + os.path.sep + args.model_name + os.path.sep  + 'abstract' + '.txt', 'a+')
+    csv_output = filepath + args.dataset + os.path.sep + args.model_name + os.path.sep  + 'sheet' + '.csv'
+
+    print_args(args, f_output, csv_output)
+
+
+    return args , f_output, csv_output
+    
+if __name__ == "__main__":
+    #configure the device
+    device = torch.device('cuda',0)
+    torch.cuda.set_device(device)
+
+    args , f_output, csv_output = setup()
+
+    m = globals()[args.model_name]
+
+    data_path = os.path.join(os.getcwd , 'src' + 'data' + args.dataset)
+    dls = ImageDataLoaders.from_folder(data_path, valid='val',item_tfms=RandomResizedCrop(128, min_scale=0.35), 
+    batch_tfms=Normalize())
+    
+    start_time = time.time()
+    log = []
+    training_time = []
+
+    for run in range(args.repetitions):
+        model = m()
+        learn = Learner(dls, model,  loss_func= nn.CrossEntropyLoss() , metrics= accuracy,  cbs = CSVLogger(csv_output,append = True))
+        if run == 0 :
+            write_new_line(csv_output, args)
+
+        learn.fit_one_cycle(args.num_epochs, max_lr =args.learning_rate)
+        if run == 0 : print('      |           '.join((learn.recorder.metric_names).map(to_string)) , file = f_output)
+        log.append(list(to_int(learn.recorder.log)))
+        print('      |        '.join((learn.recorder.log).map(to_string)), file = f_output )
+        training_time.append(time.time() - start_time)        
+    
+    exe_time = time.time() - start_time
+    
+    write_stats(args, f_output, log , training_time, exe_time)
+    
+    f_output.close()
+ 
+    
+    
+
+
+
+
+
+
+        
+
